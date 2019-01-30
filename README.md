f2aa5752ca2c400831610bdebf0b77d44bee76a4
Md Shayon <mdshayon0@gmail.com>
2019-01-30 22:15:00 +0600

    Apollo client sutup

---
+++ README.md
@@ -0,0 +1,10 @@
+#client
+
+#rest api
+In rest api we use axios or fetch to grab data
+
+#graphql
+Some GraphQL client we can use to grab data Apollo
+
+#apollo installation
+https://www.apollographql.com/docs/react/essentials/get-started.html
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo'; //BIND APOLLO WITH REACT . HELP REACT TO UNDERSTAND APOLLO
+
 import BookList from './components/BookList';
+
+//APOLLO CLIENT SETUP
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
 
 
 class App extends Component {
   render() {
     return (
-      <div id='main'>
-       <h1>Hello World</h1>
-       <BookList />
-      </div>
+      <ApolloProvider client={client} >
+        <div id='main'>
+          <h1>Hello World</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
