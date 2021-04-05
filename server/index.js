5619a0379139023b080202ea2e73153a54ae250b
Md Shayon <mdshayon0@gmail.com>
2021-04-05 10:02:41 +0600

    running simple server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
+require('dotenv').config({ path: "./config/.env" });
 const express = require('express');
 const app = express();
 
 
 app.get('/api', (req, res, next) => {
-    res.status(200).json({ "msg": "Hello world" });
+    // res.status(200).json({ "msg": "Hello world" });
+    res.send({ "msg": "Hello world" });
 });
 
 
 
-app.listen(4000, () => console.log("server in running on port 4000"));
+app.listen(process.env.PORT, () => console.log("server in running on port 4000"));
