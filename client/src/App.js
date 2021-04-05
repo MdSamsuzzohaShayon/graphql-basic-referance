9769132cc663284fbbb13190eee7fd14716d3fdb
Md Shayon <mdshayon0@gmail.com>
2021-04-05 09:24:01 +0600

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
-import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from 'react-apollo'; //BIND APOLLO WITH REACT . HELP REACT TO UNDERSTAND APOLLO
 
-import BookList from './components/BookList';
-import AddBook from "./components/AddBook";
-
-//APOLLO CLIENT SETUP
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-});
 
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client} >
-        <div id='main'>
-          <h1>List from MongoDB</h1>
-          <ul>
-          </ul>
-          <BookList />
-          <AddBook />
-        </div>
-      </ApolloProvider>
+      <div>
+        Simple component
+      </div>
     );
   }
 }
